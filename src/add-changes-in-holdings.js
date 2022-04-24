ecf5e0fce19ffd8314d846075119a5c5bc3c7603
Frederik Dudzik <frederik@dudzik.co>
2022-04-24 16:31:48 -0700

    add drift calulation

--- src/add-changes-in-holdings.js
+++ src/add-changes-in-holdings.js
@@ -1,4 +1,5 @@
-import { crypto, base, basisPoint } from './currency.js'
+import currency from 'currency.js'
+import { crypto, base, basisPoint, percentage } from './currency.js'
 
 const calculateAmount = (coin, funds) => {
   const distribution = basisPoint(coin.distribution).divide(100.0)
@@ -19,6 +20,21 @@ const changesToAmount = (coin, currentHoldings) => {
   return coin
 }
 
+const calculateDrift = (entries) => {
+    const totalAmount = entries.reduce((total, entry) => entry.amountBaseCurrency.add(total), currency(0.0))
+    const totalChange = entries.reduce((total, entry) => {
+      const absChange = Math.abs(entry.changeBaseCurrency.value)
+      return currency(absChange).add(total)
+    }, currency(0.0))
+    const changePercentage = percentage(totalChange.multiply(100).divide(totalAmount))
+
+    return {
+      totalAmount: totalAmount.format(),
+      totalChange: totalChange.format(),
+      changePercentage: changePercentage.format()
+    }
+}
+
 export default (obj) => {
     obj.entries = obj.entries
                      .map((coin) => calculateAmount(coin, obj.funds))
@@ -32,6 +48,7 @@ export const marshal = (strategy, marshalCoin) => {
     return { 
       dominance: result.totalDominance.format(),
       meta: { ...strategy.marshal(result), strategyID: strategy.id },
+      drift: calculateDrift(result.entries),
       entriesCount: result.entries.length,
       entries: result.entries.map(marshalCoin),
     }
